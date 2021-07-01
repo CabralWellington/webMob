09098c1a479c44f2aa833e9d296ce99ad17b2690
Wellington <wellington_oliveira@live.com>
2021-07-01 16:47:20 -0400

    first commit

---
+++ db/db.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2/promise");
+async function connect(){
+    if(global.connection && global.connection.state !== 'disconnected'){
+        console.log("MySql Connection Recovered");
+        return global.connection;
+    }
+    const connection = await mysql.createConnection("mysql://Admin:Admin@123@grafana-server:3306/_mysql");
+    console.log("MySql Connection Started");
+    global.connection = connection;
+    return connection;
+}
+connect();
+module.exports = {connect}
